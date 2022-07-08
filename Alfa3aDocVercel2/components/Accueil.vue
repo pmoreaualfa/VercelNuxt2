a131901ee0000ca2d93377eb0c1a73811620b255
pmoreaualfa <philippe.moreau@alfa3a.org>
2022-07-08 18:20:28 +0200

    after reset

--- Alfa3aDocVercel2/components/Accueil.vue
+++ Alfa3aDocVercel2/components/Accueil.vue
@@ -13,7 +13,7 @@
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
-          Bienvenue à tous
+          Bienvenue
         </h2>
         <p class="mt-3 text-gray-600">
           Service Système d'information.
